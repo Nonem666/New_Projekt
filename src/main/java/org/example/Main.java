322af70d328830c9ed53f52e26e1f0e75c099db8
Nonem666 <vladoskrutos59@gmail.com>
2023-05-30 21:52:03 +0300

    xdx

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    static final Logger rootLogger = LogManager.getRootLogger();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        rootLogger.info("info message");
+
+        // debug
+        if (rootLogger.isDebugEnabled()) {
+            rootLogger.debug("In debug message");
+        }
+        try {
+            throw new NullPointerException("Null!!!");
+        } catch (NullPointerException ex) {
+            rootLogger.error("error message: " + ex.getMessage());
+            rootLogger.fatal("fatal error message: " + ex.getMessage());
+        }
     }
 }
